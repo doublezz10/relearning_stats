a5e567ea07f2a82b24e8c2c2f96cacbb6542b15f
Zach Laptop <zachzeisler1@gmail.com>
2020-09-21 19:04:33 -0400

    added plots comparing priors and posteriors

--- Lecture2.r
+++ Lecture2.r
@@ -26,6 +26,20 @@ fit3 <- brm(height ~ 1, data=Howell1, family="normal",
             ))
 summary(fit3)
 
+# Plot the bad prior and the posterior
+
+plot(seq(0, 300, length.out=1000), 
+     dnorm(seq(0, 300, length.out=1000), mean=0, sd=10),
+     add = TRUE, type='l', xlim=c(0, 900))
+hist(extract_draws(fit3)$dpars$sigma, breaks=50,prob=TRUE,add=TRUE)
+
+# What should this look like?
+
+plot(seq(0, 300, length.out=1000), 
+     dnorm(seq(0, 300, length.out=1000), mean=0, sd=30),
+     add = TRUE, type='l', xlim=c(0, 90))
+hist(extract_draws(fit2)$dpars$sigma, breaks=50,prob=TRUE,add=TRUE)
+
 # Add a covariate (and its prior)
 
 fit4 <- brm(height ~ weight, data=Howell1, family="normal",
