efb2d51c5d07b0c6ad4256017185f34ae1fdfa13
Zach <zachzeisler1@gmail.com>
2020-09-21 16:50:34 -0400

    added code from Lecture 2 - exploring doing linear regression using Bayesian modelling

---
+++ Lecture2.r
@@ -0,0 +1,38 @@
+data(Howell1,package='rethinking')
+
+install.packages('brms')
+library(brms)
+
+# Fit a Bayesian model without setting priors (it'll do the median for us)
+
+fit <- brm(height ~ 1, data=Howell1, family="normal")
+
+summary(fit)
+
+prior_summary(fit)
+
+# Fit another model by setting priors
+
+fit2 <- brm(height ~ 1, data=Howell1, family="normal",
+           prior = c(prior(normal(160,20),class="Intercept"),
+                     prior(normal(0,30),class="sigma")
+                     ))
+summary(fit2)
+
+# Use a bad prior!
+
+fit3 <- brm(height ~ 1, data=Howell1, family="normal",
+            prior = c(prior(normal(3600,10),class="Intercept"),
+                      prior(normal(0,10),class="sigma")
+            ))
+summary(fit3)
+
+# Add a covariate (and its prior)
+
+fit4 <- brm(height ~ weight, data=Howell1, family="normal",
+            prior = c(prior(normal(160,20),class="Intercept"),
+                      prior(normal(0,30),class="sigma"),
+                      #prior(normal(0,30),coef="weight"), #prior only on weight
+                      prior(normal(0,20),class="b") #prior on all regression weights
+            ))
+summary(fit4)
